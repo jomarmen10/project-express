a550ae14491497b56b389fe265a799fa29b362d1
Jomar Mendoza <jmm21@Jomars-MacBook-Pro.local>
2019-05-15 12:13:32 -0700

    create route woriking on postman

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const bycrpt = require('bcrypt')
+
+
+const User = require('../models/User')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res) => {
+  return res.json({data: 'Received a GET HTTP method users'});
 });
+
+router.post('/', async(req, res) => {
+ try {
+   const user = await User.create(req.body)
+   res.json({user})
+ } catch(err) {
+   res.json({err})
+ }
+});
+
+
+
+
+
 
 module.exports = router;
