a9bcbd128bdd09f67e83c9a9ef936bfbf268b800
Jomar Mendoza <jmm21@Jomars-MacBook-Pro.local>
2019-05-15 10:38:54 -0700

    still fixing the express folder

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  firstName: { type: String, required: true, },
+  lastName: { type: String, required: true, },
+  email: { type: String, required: true, },
+  password: { type: String, required: true, },
+  isDeleted: { type: Boolean, default: false },
+});
+
+UserSchema.methods.generateHash = (password) => {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
+};
+
+UserSchema.methods.validPassword = (password) => {
+  return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema)
